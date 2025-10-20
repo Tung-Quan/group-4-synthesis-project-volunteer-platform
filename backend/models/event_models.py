8d275c25b7fc14603a5b62f299ef5ff626707692
quangMinh9305 <quang.nguyenquang9305@hcmut.edu.vn>
2025-10-20 19:28:01 +0700

    Update event APIs

--- backend/models/event_models.py
+++ backend/models/event_models.py
@@ -18,3 +18,22 @@ class UpdateEventRequest(BaseModel):
     ends_at: str    # ISO 8601 format
     capacity: int
     organizer_id: str
+
+class ApplyEvent(BaseModel):
+    event_id: str
+    applicant_id: str
+    name: str
+    department: str
+    email: str
+    phone_no: str 
+
+class CheckingAttendance(BaseModel):
+    organizer_id: str
+    applicant_id: str
+    attended: bool = True
+
+class ReviewApplication(BaseModel):
+    organizer_id: str
+    application_id: str
+    approve: bool 
+    reason: str | None = None
--- backend/routes/events_route.py
+++ backend/routes/events_route.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException, status, Request
-from ..models.event_models import CreateEventRequest, UpdateEventRequest
-from ..controllers.event_controller import events, create_event, update_event
+from ..models.event_models import *
+from ..controllers.event_controller import *
 
 router = APIRouter()
 
@@ -21,6 +21,27 @@ def post_event(request: CreateEventRequest):
 @router.put("/")
 def put_event(request: UpdateEventRequest, req: Request):
     result, status_code = update_event(request, req)
+    if status_code != 200:
+        raise HTTPException(status_code=status_code, detail=result["message"])
+    return result
+
+@router.post("/")
+def regis_event(request: ApplyEvent):
+    result, status_code = apply_event(request)
+    if status_code != 201:
+        raise HTTPException(status_code=status_code, detail=result["message"])
+    return result
+
+@router.post("/")
+def attendance_confirm(request: CheckingAttendance):
+    result, status_code = check_attendance(request)
+    if status_code != 200:
+        raise HTTPException(status_code=status_code, detail=result["message"])
+    return result
+
+@router.post("/")
+def review_app(request: ReviewApplication):
+    result, status_code = review_application(request)
     if status_code != 200:
         raise HTTPException(status_code=status_code, detail=result["message"])
     return result
